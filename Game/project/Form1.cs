9491bfcad9ce8072ac8fb0a9db065ea7805e5384
Annie Phan <hoang.phan.anna@gmail.com>
2019-12-30 21:13:15 -0800

    Game files updated

---
+++ Game/project/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += new System.EventHandler(SelectButton);
+            button2.Click += new System.EventHandler(SelectButton);
+            button3.Click += new System.EventHandler(SelectButton);
+            button4.Click += new System.EventHandler(SelectButton);
+            button5.Click += new System.EventHandler(SelectButton);
+            button6.Click += new System.EventHandler(SelectButton);
+            button7.Click += new System.EventHandler(SelectButton);
+            button8.Click += new System.EventHandler(SelectButton);
+            button9.Click += new System.EventHandler(SelectButton);
+        }
+        //Declare variables
+        int[] arraynumber = new int[9];
+        int dicesum,usersumtotal;
+        int buttonnumber1;
+        int die1, die2;
+        bool buttonwasclicked = false;
+        bool[] colorChangedAlready = { false, false, false, false, false, false, false, false, false };
+
+        //This method rolls the dices, which also call the other method to update labels and update pictures boxes
+        //This is taken from previous assignment
+        private void RollDice(out int die1, out int die2)
+        {
+            Random random = new Random();
+            die1 = random.Next(1, 7);
+            die2 = random.Next(1, 7);
+
+            UpdateLabel(die1, die2);
+            UpdatePictureBoxes(die1, die2);
+        }
+
+        //This method update the labels
+        //This is taken from previous assignment
+        private void UpdateLabel(int die1, int die2)
+        {
+            dicesum = die1 + die2;
+            lbSum.Text = dicesum.ToString("Sum = " + "#.##");
+        }
+
+        //This method call on the updatePictureBox to update dice images
+        //This is taken from previous assignment
+        private void UpdatePictureBoxes(int die1, int die2)
+        {
+            UpdatePictureBox(pictureBox1, die1);
+            UpdatePictureBox(pictureBox2, die2);
+        }
+
+        //This method change the dice images
+        //This is taken from previous assignment
+        private void UpdatePictureBox(PictureBox pb, int die)
+        {
+            switch (die)
+            {
+                case 1: pb.Image = Project.Properties.Resources.die1; break;
+                case 2: pb.Image = Project.Properties.Resources.die2; break;
+                case 3: pb.Image = Project.Properties.Resources.die3; break;
+                case 4: pb.Image = Project.Properties.Resources.die4; break;
+                case 5: pb.Image = Project.Properties.Resources.die5; break;
+                case 6: pb.Image = Project.Properties.Resources.die6; break;
+            }
+        }
+
+        //This method convert to whatever button into an variable and past it though a Sum method
+        public void SelectButton(object sender, EventArgs e)
+        {
+             
+            buttonnumber1 = int.Parse((sender as Button).Text);
+            lbButtonSelected.Text = buttonnumber1.ToString("#.##");
+            TotalingButtonSum(buttonnumber1);
+ 
+        }
+
+        //This method adds up the sum from the user's choices 
+        public void TotalingButtonSum(int buttonnumber)
+        {
+            //If the button is press, the button's number goes into a switch
+            if (buttonwasclicked)
+            {
+                switch (buttonnumber)
+                {
+                    case 1: arraynumber[0] = 1; break;
+                    case 2: arraynumber[1] = 2; break;
+                    case 3: arraynumber[2] = 3; break;
+                    case 4: arraynumber[3] = 4; break;
+                    case 5: arraynumber[4] = 5; break;
+                    case 6: arraynumber[5] = 6; break;
+                    case 7: arraynumber[6] = 7; break;
+                    case 8: arraynumber[7] = 8; break;
+                    case 9: arraynumber[8] = 9; break;
+                }
+            }
+            //If the user disselect the button, the button's number is put in the array as zero
+            else
+            {
+                switch (buttonnumber) { 
+                    case 1: arraynumber[0] = 0; break;
+                    case 2: arraynumber[1] = 0; break;
+                    case 3: arraynumber[2] = 0; break;
+                    case 4: arraynumber[3] = 0; break;
+                    case 5: arraynumber[4] = 0; break;
+                    case 6: arraynumber[5] = 0; break;
+                    case 7: arraynumber[6] = 0; break;
+                    case 8: arraynumber[7] = 0; break;
+                    case 9: arraynumber[8] = 0; break;
+            }
+            }
+            //This loops into the array and sum up the total in the array
+            int usersum =0;
+            for (int index = 0; index < arraynumber.Length; index++)
+            {
+                usersum += arraynumber[index];
+            }
+            
+            lbUsersum.Text = usersum.ToString("#.##");
+            usersumtotal = usersum;
+
+
+        }
+        //This switch toggle from light grey and light green depending if the user has select or disselect the button
+        //This was taken from demo that was shown in last class 
+        private void ColourChangeButton(int buttoncode)
+        {
+            int index = buttoncode - 1;
+            Color ButtonColour;
+            if (colorChangedAlready[index])
+            {
+                ButtonColour = SystemColors.ControlLight;
+                colorChangedAlready[index] = false;
+                buttonwasclicked = false;
+            }
+            else
+            {
+                ButtonColour = Color.LightGreen;
+                colorChangedAlready[index] = true;
+                buttonwasclicked = true;
+            }
+
+            switch (buttoncode)
+            {
+                case 1: button1.BackColor = ButtonColour; break;
+                case 2: button2.BackColor = ButtonColour; break;
+                case 3: button3.BackColor = ButtonColour; break;
+                case 4: button4.BackColor = ButtonColour; break;
+                case 5: button5.BackColor = ButtonColour; break;
+                case 6: button6.BackColor = ButtonColour; break;
+                case 7: button7.BackColor = ButtonColour; break;
+                case 8: button8.BackColor = ButtonColour; break;
+                case 9: button9.BackColor = ButtonColour; break;
+            }
+        }
+        //This method elminate the button and makes them unclickable
+        private void ElminateButton(Array buttonnumber)
+        {
+            foreach (int element in arraynumber)
+            {
+                switch (element)
+                {
+                    case 1: button1.BackColor = Color.DarkGreen; button1.Enabled = false; break;
+                    case 2: button2.BackColor = Color.DarkGreen; button2.Enabled = false; break;
+                    case 3: button3.BackColor = Color.DarkGreen; button3.Enabled = false; break;
+                    case 4: button4.BackColor = Color.DarkGreen; button4.Enabled = false; break;
+                    case 5: button5.BackColor = Color.DarkGreen; button5.Enabled = false; break;
+                    case 6: button6.BackColor = Color.DarkGreen; button6.Enabled = false; break;
+                    case 7: button7.BackColor = Color.DarkGreen; button7.Enabled = false; break;
+                    case 8: button8.BackColor = Color.DarkGreen; button8.Enabled = false; break;
+                    case 9: button9.BackColor = Color.DarkGreen; button9.Enabled = false; break;
+                }
+            }
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ColourChangeButton(1);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ColourChangeButton(9);
+        }
+
+        private void lbUsersum_Click(object sender, EventArgs e)
+        {
+
+        }
+        //This clears everything
+        private void bttnNewGame_Click(object sender, EventArgs e)
+        {
+            lbSum.Text = "";
+            lbUsersum.Text = "";
+            lbButtonSelected.Text = "";
+            dicesum = 0;
+            usersumtotal = 0;
+            button1.BackColor = SystemColors.ControlLight; button1.Enabled = true;
+            button2.BackColor = SystemColors.ControlLight; button2.Enabled = true; 
+            button3.BackColor = SystemColors.ControlLight; button3.Enabled = true; 
+            button4.BackColor = SystemColors.ControlLight; button4.Enabled = true; 
+            button5.BackColor = SystemColors.ControlLight; button5.Enabled = true; 
+            button6.BackColor = SystemColors.ControlLight; button6.Enabled = true; 
+            button7.BackColor = SystemColors.ControlLight; button7.Enabled = true; 
+            button8.BackColor = SystemColors.ControlLight; button8.Enabled = true; 
+            button9.BackColor = SystemColors.ControlLight; button9.Enabled = true;
+
+        }
+        private void lbButtonSelected_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            //If the dice sum is equal to the user's sum it will goes into an if else switch
+            if (dicesum == usersumtotal)
+            {
+               RollDice(out die1, out die2);
+               lbUsersum.Text = usersumtotal.ToString("#.##");
+               ElminateButton(arraynumber);
+            }
+            //if the dice sum and user's sum don't match the button will go back into light grey button
+            else 
+            {
+                foreach (int element in arraynumber)
+                {
+                    switch (element)
+                    {
+                        case 1: button1.BackColor = SystemColors.ControlLight; break;
+                        case 2: button2.BackColor = SystemColors.ControlLight; break;
+                        case 3: button3.BackColor = SystemColors.ControlLight; break;
+                        case 4: button4.BackColor = SystemColors.ControlLight; break;
+                        case 5: button5.BackColor = SystemColors.ControlLight; break;
+                        case 6: button6.BackColor = SystemColors.ControlLight; break;
+                        case 7: button7.BackColor = SystemColors.ControlLight; break;
+                        case 8: button8.BackColor = SystemColors.ControlLight; break;
+                        case 9: button9.BackColor = SystemColors.ControlLight; break;
+                    }
+                }
+            }
+
+            //This reset the array when the loop has ended
+            lbButtonSelected.Text  = "";
+            lbUsersum.Text = "";
+            Array.Clear(arraynumber, 0, arraynumber.Length);
+
+        }
+    }
+}
---
+++ Game/web/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bttnNewGame = new System.Windows.Forms.Button();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbSum = new System.Windows.Forms.Label();
+            this.lbButtonSelected = new System.Windows.Forms.Label();
+            this.lbUsersum = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(33, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 65);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(109, 145);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 65);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(185, 145);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 65);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(33, 216);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 65);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(109, 216);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 65);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(185, 216);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 65);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bttnNewGame);
+            this.groupBox1.Controls.Add(this.btnRoll);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.lbSum);
+            this.groupBox1.Location = new System.Drawing.Point(280, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 316);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Box";
+            // 
+            // bttnNewGame
+            // 
+            this.bttnNewGame.Location = new System.Drawing.Point(20, 250);
+            this.bttnNewGame.Name = "bttnNewGame";
+            this.bttnNewGame.Size = new System.Drawing.Size(241, 48);
+            this.bttnNewGame.TabIndex = 4;
+            this.bttnNewGame.Text = "New Game";
+            this.bttnNewGame.UseVisualStyleBackColor = true;
+            this.bttnNewGame.Click += new System.EventHandler(this.bttnNewGame_Click);
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(20, 196);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(241, 48);
+            this.btnRoll.TabIndex = 3;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Project.Properties.Resources.die6;
+            this.pictureBox2.Location = new System.Drawing.Point(147, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(95, 81);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project.Properties.Resources.die6;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbSum
+            // 
+            this.lbSum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbSum.Location = new System.Drawing.Point(20, 37);
+            this.lbSum.Name = "lbSum";
+            this.lbSum.Size = new System.Drawing.Size(241, 46);
+            this.lbSum.TabIndex = 0;
+            this.lbSum.Text = " ";
+            this.lbSum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbButtonSelected
+            // 
+            this.lbButtonSelected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbButtonSelected.Location = new System.Drawing.Point(21, 25);
+            this.lbButtonSelected.Name = "lbButtonSelected";
+            this.lbButtonSelected.Size = new System.Drawing.Size(148, 42);
+            this.lbButtonSelected.TabIndex = 10;
+            this.lbButtonSelected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbButtonSelected.Click += new System.EventHandler(this.lbButtonSelected_Click);
+            // 
+            // lbUsersum
+            // 
+            this.lbUsersum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbUsersum.Location = new System.Drawing.Point(20, 24);
+            this.lbUsersum.Name = "lbUsersum";
+            this.lbUsersum.Size = new System.Drawing.Size(154, 44);
+            this.lbUsersum.TabIndex = 11;
+            this.lbUsersum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbUsersum.Click += new System.EventHandler(this.lbUsersum_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbButtonSelected);
+            this.groupBox2.Location = new System.Drawing.Point(582, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 83);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Button Selected";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbUsersum);
+            this.groupBox3.Location = new System.Drawing.Point(582, 102);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(191, 89);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "User\'s Sum";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 340);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dice Game";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bttnNewGame;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbSum;
+        private System.Windows.Forms.Label lbButtonSelected;
+        private System.Windows.Forms.Label lbUsersum;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
